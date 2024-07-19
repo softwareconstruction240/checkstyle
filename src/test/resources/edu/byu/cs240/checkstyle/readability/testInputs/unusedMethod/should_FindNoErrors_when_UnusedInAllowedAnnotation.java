ef50c2651012573be7b31f9cf692f7f7693a6dc1
Michael Davenport <mdaven19@byu.edu>
2024-07-19 11:24:11 -0600

    fix unused method annotaion test case

--- src/test/resources/edu/byu/cs240/checkstyle/readability/testInputs/unusedMethod/should_FindNoErrors_when_UnusedInAllowedAnnotation.java
+++ src/test/resources/edu/byu/cs240/checkstyle/readability/testInputs/unusedMethod/should_FindNoErrors_when_UnusedInAllowedAnnotation.java
@@ -1,7 +1,7 @@
 class should_FindNoErrors_when_UnusedInAllowedAnnotation {
     @FooBar("hello")
     public void hi() {
-        hello();
+        System.out.println("hi");
     }
     @FooBar(value = "hi")
     public void hello() {
