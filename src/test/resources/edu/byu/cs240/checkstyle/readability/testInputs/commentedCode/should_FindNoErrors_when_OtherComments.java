c36a287c30669256e08fcb7297feaa77202b402d
Michael Davenport <mdaven19@byu.edu>
2025-08-11 14:08:02 -0600

    improve test case, add a comment with non-code lines ending in code characters

--- src/test/resources/edu/byu/cs240/checkstyle/readability/testInputs/commentedCode/should_FindNoErrors_when_OtherComments.java
+++ src/test/resources/edu/byu/cs240/checkstyle/readability/testInputs/commentedCode/should_FindNoErrors_when_OtherComments.java
@@ -9,4 +9,23 @@ class should_FindNoErrors_when_OtherComments {
         int j = 4;
         System.out.println(i + j);
     }
+
+    /**
+     * Lol idk seemed like a fun time;
+     * Javadoc comments can be weird sometimes;
+     * Documentation is great to have;
+     * but seems like most devs don't like writing it;
+     *
+     * @param a yes (extra details)
+     * @param b no (but is it actually?)
+     * @param c sometimes, but semicolons;
+     * @param d occasionally,
+     * @param e often (
+     * @param f almost always /
+     * @param g literally never }
+     * @param h the only one actually used lol {
+     */
+    public void hi(int a, int b, Object c, char d, boolean e, long f, Void g, String h) {
+        System.out.println(h + " lol!");
+    }
 }
