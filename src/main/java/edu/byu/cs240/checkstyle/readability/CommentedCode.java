a14137a15a872de622b636729488de364dea68f1
Michael Davenport <mdaven19@byu.edu>
2025-08-11 18:05:46 -0600

    rework commented code check

--- src/main/java/edu/byu/cs240/checkstyle/readability/CommentedCode.java
+++ src/main/java/edu/byu/cs240/checkstyle/readability/CommentedCode.java
@@ -4,10 +4,8 @@ import com.puppycrawl.tools.checkstyle.api.AbstractCheck;
 import com.puppycrawl.tools.checkstyle.api.DetailAST;
 import com.puppycrawl.tools.checkstyle.api.TokenTypes;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.regex.Pattern;
 
 /**
  * Reports successive lines of commented out code
@@ -16,9 +14,51 @@ import java.util.Set;
  */
 public class CommentedCode extends AbstractCheck {
 
-    private static final Set<Character> CODE_LINE_END_CHARS = Set.of(';', ',', '{', '}', '(', ')', '/');
+    private static final Map<Pattern, Double> ENTIRE_COMMENT_CODE_PATTERNS = new HashMap<>();
+    private static final Map<Pattern, Double> SINGLE_LINE_CODE_PATTERNS = new HashMap<>();
+    private static final Map<Pattern, Double> ENTIRE_COMMENT_NONCODE_PATTERNS = new HashMap<>();
+    private static final Map<Pattern, Double> SINGLE_LINE_NONCODE_PATTERNS = new HashMap<>();
+
+    private static final Set<String> JAVA_RESERVED_WORDS =
+            Set.of("abstract", "continue", "for", "new", "switch", "assert", "default", "package", "synchronized",
+                    "boolean", "do", "if", "private", "this", "break", "double", "implements", "protected", "throw",
+                    "byte", "else", "import", "public", "throws", "case", "enum", "instanceof", "return", "transient",
+                    "catch", "extends", "int", "short", "try", "char", "final", "interface", "static", "void", "class",
+                    "finally", "long", "strictfp", "volatile", "const", "float", "native", "super", "while");
+
+    static {
+        ENTIRE_COMMENT_CODE_PATTERNS.put(Pattern.compile("[\\s\\S]*(?:if|for|while)\\s*\\([\\s\\S]+\\)\\s*\\{[\\s\\S]*}[\\s\\S]*"), 0.9); //simple if/for/while
+        ENTIRE_COMMENT_CODE_PATTERNS.put(Pattern.compile("[\\s\\S]*if\\s*\\([\\s\\S]+\\)\\s*\\{[\\s\\S]*}\\s*else(?:\\sif\\s*\\([\\s\\S]+\\))?\\s*\\{[\\s\\S]*}[\\s\\S]*"), 0.95); //if/else or if/elseif
+        ENTIRE_COMMENT_CODE_PATTERNS.put(Pattern.compile("^(?:[\\s\\S]*\\n\\s*)?(?:private|public|protected)?\\s+\\w+\\s+\\w+\\s*\\([\\s\\S]*\\)\\s*\\{[\\s\\S]*}[\\s\\S]*$"), 0.9); //method declaration
+
+        SINGLE_LINE_CODE_PATTERNS.put(Pattern.compile("^(?:if|while)\\s*\\([\\s\\S]+\\)\\s*\\{?\\s$"), 0.85); //simple if/while
+        SINGLE_LINE_CODE_PATTERNS.put(Pattern.compile("^for\\s*\\([\\s\\S]*;[\\s\\S]*;[\\s\\S]*\\)\\s*\\{?\\s$"), 0.9); //start of for loop
+        SINGLE_LINE_CODE_PATTERNS.put(Pattern.compile("^\\w+\\s+\\w+\\s*=[\\s\\S]*$"), 0.9); //variable declaration
+        SINGLE_LINE_CODE_PATTERNS.put(Pattern.compile("^\\w+\\s*(?:\\+|-|\\*|/|%|&|\\^|\\||<<|>>|>>>)?=[\\s\\S]*$"), 0.85); //variable assignment
+        SINGLE_LINE_CODE_PATTERNS.put(Pattern.compile("^(?:\\w+(?:\\(.*\\))?\\.)*\\w+\\(.*\\);$"), 0.85); //method call
+        SINGLE_LINE_CODE_PATTERNS.put(Pattern.compile("^return\\s+.*;$"), 0.9); // return statements
+        SINGLE_LINE_CODE_PATTERNS.put(Pattern.compile("^\\w+(?:--|\\+\\+);?$"), 0.9); //increment/decrement
+        SINGLE_LINE_CODE_PATTERNS.put(Pattern.compile("^.*;$"), 0.8);   //end semicolon
+        SINGLE_LINE_CODE_PATTERNS.put(Pattern.compile("^}$"), 0.6);   //only end curly
+
+
+        ENTIRE_COMMENT_NONCODE_PATTERNS.put(Pattern.compile("^(?:\\*.*\n)*\\*.*?$"), 0.85); //Javadoc comment
+        ENTIRE_COMMENT_NONCODE_PATTERNS.put(Pattern.compile("^\\{(?:\\s*\"\\w+\":\\s*(?:\"?\\w+\"?|\"\"|\\[[\\s\\S]*]|\\{[\\s\\S]*}),?\\s*)+}$"), 0.75); //JSON object
+
+        SINGLE_LINE_NONCODE_PATTERNS.put(Pattern.compile("^\\*.*$"), 0.65); //start of javadoc comment line
+        SINGLE_LINE_NONCODE_PATTERNS.put(Pattern.compile("^\\*\\s+@(?:param|return|throws)\\s+.*$"), 0.8); //start of javadoc comment line
+        SINGLE_LINE_NONCODE_PATTERNS.put(Pattern.compile("^\"\\w+\":\\s*(?:\"?[\\w: ]+\"?|\"\"),?$"), 0.65); //individual line of json field
+        SINGLE_LINE_NONCODE_PATTERNS.put(Pattern.compile("^(?i)(?:TODO|FIXME):.*$"), 0.8);
+        SINGLE_LINE_NONCODE_PATTERNS.put(Pattern.compile("^NOTE:.*$"), 0.7);
+    }
 
     private int min = 5;
+
+    private double minConfidence = 0.5;
+
+    private int entireCommentWeight = 15;
+    private int singleLineWeight = 7;
+    private int reservedWordWeight = 2;
 
     private final List<CommentNode> commentNodes = new ArrayList<>();
 
@@ -29,6 +69,27 @@ public class CommentedCode extends AbstractCheck {
      */
     public void setMin(int min) {
         this.min = min;
+    }
+
+    /**
+     * Sets the minimum confidence a selection of comments should be before reporting
+     *
+     * @param minConfidence minimum proportion
+     */
+    public void setMinConfidence(double minConfidence) {
+        this.minConfidence = minConfidence;
+    }
+
+    public void setEntireCommentWeight(int entireCommentWeight) {
+        this.entireCommentWeight = entireCommentWeight;
+    }
+
+    public void setReservedWordWeight(int reservedWordWeight) {
+        this.reservedWordWeight = reservedWordWeight;
+    }
+
+    public void setSingleLineWeight(int singleLineWeight) {
+        this.singleLineWeight = singleLineWeight;
     }
 
     @Override
@@ -76,24 +137,66 @@ public class CommentedCode extends AbstractCheck {
             return;
         }
 
-        int numSuccessiveLines = 0;
-        for(String comment : commentLines) {
-            if(comment.isBlank() || CODE_LINE_END_CHARS.contains(comment.charAt(comment.length() - 1))) {
-                numSuccessiveLines++;
-            }
-            else {
-//            int numSuccessiveLines = commentNodes.getLast().getLastLineNo() + 1 - commentNodes.getFirst().ast().getLineNo();
-                if(numSuccessiveLines >= min) {
-                    log(commentNodes.getFirst().ast(), String.format("%d lines of commented out code", numSuccessiveLines));
-                }
-                numSuccessiveLines = 0;
-            }
+        String entireComment = String.join("\n", commentLines);
+        double entireCommentCodeScore = maxMatcher(entireComment, ENTIRE_COMMENT_CODE_PATTERNS);
+        double entireCommentNonCodeScore = maxMatcher(entireComment, ENTIRE_COMMENT_NONCODE_PATTERNS);
+        double entireCommentScore = (entireCommentCodeScore - entireCommentNonCodeScore) * entireCommentWeight;
+
+        double singleLineCodeScore = maxMatcher(commentLines, SINGLE_LINE_CODE_PATTERNS);
+        double singleLineNonCodeScore = maxMatcher(commentLines, SINGLE_LINE_NONCODE_PATTERNS);
+        double singleLineScore = (singleLineCodeScore - singleLineNonCodeScore) * singleLineWeight;
+
+        String[] allWords = entireComment.toLowerCase().split("\\s+");
+        long reservedWordCount = Arrays.stream(allWords).filter(JAVA_RESERVED_WORDS::contains).count();
+        double reservedWordScore = (double) reservedWordCount / allWords.length * reservedWordWeight;
+
+        int totalWeight = reservedWordWeight;
+        double totalScore = reservedWordScore;
+        if(entireCommentScore != 0) {
+            totalWeight += entireCommentWeight;
+            totalScore += entireCommentScore;
         }
-        if(numSuccessiveLines >= min) {
-            log(commentNodes.getFirst().ast(), String.format("%d lines of commented out code", numSuccessiveLines));
+        if(singleLineScore != 0) {
+            totalWeight += singleLineWeight;
+            totalScore += singleLineScore;
+        }
+
+        double totalConfidence = totalScore / totalWeight;
+
+        if (totalConfidence >= minConfidence) {
+            log(commentNodes.getFirst().ast(), String.format("%d comment lines likely commented code (%d%% confidence)",
+                    commentNodes.getLast().getLastLineNo() + 1 - commentNodes.getFirst().ast().getLineNo(),
+                    Math.round(totalConfidence * 100)));
         }
 
         commentNodes.clear();
+    }
+
+    private double maxMatcher(String match, Map<Pattern, Double> patternScores) {
+        double max = 0;
+        for(var entry : patternScores.entrySet()) {
+            if(entry.getValue() > max && entry.getKey().matcher(match).matches()) {
+                max = entry.getValue();
+            }
+        }
+        return max;
+    }
+
+    private double maxMatcher(List<String> matches, Map<Pattern, Double> patternScores) {
+        if(matches.isEmpty()) {
+            return 0;
+        }
+
+        double totalScore = 0;
+        int totalNonBlankLines = 0;
+
+        for(String match : matches) {
+            if(!match.isBlank()) {
+                totalScore += maxMatcher(match, patternScores);
+                totalNonBlankLines++;
+            }
+        }
+        return totalNonBlankLines == 0 ? 0 : totalScore / totalNonBlankLines;
     }
 
     private record CommentNode(DetailAST ast, List<String> lines) {
